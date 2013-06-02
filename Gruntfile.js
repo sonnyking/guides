1029cf966b7472e258cc8ae9a93cf1d9f8906d52
Sonny King <sonny@Sonnys-MacBook-Pro-2.local>
2013-06-02 11:24:30 -0700

    adding test harness

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,22 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jasmine : {
+      src : 'test/index.html',
+      options : {
+        specs : 'test/spec/**/*.js'
+      }
+    },
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'src/**/*.js',
+        'test/spec/**/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
     watch: {
       files: ['./src/**/*.js', './src/**/*.html'],
       tasks: 'default'
@@ -29,7 +45,8 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.registerTask('default', ['server']);
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.registerTask('watch', ['watch']);
   grunt.registerTask('sass', ['sass']);
   grunt.registerTask('server', 'Start and hold a web server open.', function() {
@@ -37,5 +54,6 @@ module.exports = function(grunt) {
     grunt.log.writeln('Starting web server on port 3502.');
     require('./lib/server.js').listen(3502).on('close', done);
   });
-
+  grunt.registerTask('test', ['jasmine']);
+  grunt.registerTask('default', ['test']);
 };
---
+++ test/spec/guides.js
@@ -0,0 +1,7 @@
+describe("Guides", function() {
+
+  it("I don't know what to test", function() {
+    expect(true).toBe(true);
+  });
+
+});
